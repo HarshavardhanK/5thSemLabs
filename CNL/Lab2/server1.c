be4fa8574e4dede4e1a8bc84058fa752df171c22
Harshavardhan K <harshavardhan816@gmail.com>
2019-08-19 00:09:11 +0530

    lab2 server 1 update

--- CNL/Lab2/server1.c
+++ CNL/Lab2/server1.c
@@ -1,48 +1,74 @@
 #include<stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+
 #define PORTNO 10200
 
-void PerformServerTask(int *newsockfd) {
-	int n;
-	char buf[256] = "";
-	n = read(*newsockfd, buf, sizeof(buf));
-	printf("\nMessage from Client %s\n", buf);
-	n = write(*newsockfd, buf, sizeof(buf));
-	close(*newsockfd);
-	exit(0);
+void server_task(int *sockfd, int *newsockfd, char*ip, unsigned short int port) {
+
+	char buffer[256];
+
+	struct sockaddr_in client;
+	client.sin_family = AF_INET;
+	client.sin_port = htons(port);
+	client.sin_addr.s_addr = inet_addr(ip);
+
+	socklen_t client_len = sizeof(client_len);
+
+	*newsockfd = accept(*sockfd, (struct sockaddr*) &client, &client_len);
+
+	read(*newsockfd, buffer, sizeof(buffer));
+
+	printf("\nMessage from clien is %s\n", buffer);
+
+	write(*newsockfd, buffer, sizeof(buffer));
+
 }
 
-void CreateServerSocket() {
-	int sockfd, newsockfd, portno, clilen, n = 1;
-	struct sockaddr_in seraddr, cliaddr;
-	int i, value;
-	char buf[256];
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	seraddr.sin_family = AF_INET;
-	seraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	seraddr.sin_port = htons(PORTNO);
-	bind(sockfd, (struct sockaddr *)&seraddr, sizeof(seraddr));
+void create_server(char* ip, unsigned short int port) {
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = inet_addr(ip);
+
+	if(bind(sockfd, (struct sockaddr*) &server, sizeof(server)) < 0) {
+		printf("cannot bind\n");
+		exit(-1);
+	}
+
 	listen(sockfd, 5);
+
+	int newsockfd;
+
 	while(1) {
-		
-		clilen = sizeof(clilen);
-		newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
-		if (fork() == 0) {
-			PerformServerTask(&newsockfd);
-		}
-		else {
+
+		if(fork() == 0) {
+
+			server_task(&sockfd, &newsockfd, ip, port);
+
+			close(newsockfd);
+
+			exit(0);
+
+		} else {
+
 			close(newsockfd);
 		}
 	}
 }
 
-int main()
-{
-	CreateServerSocket();
+int main() {
+
+	create_server("127.0.0.1", 3000);
+
 	return 0;
 }
