56f79e13315113fda617ab61dbf9a701735d3416
tsarshah <harshavardhan816@gmail.com>
2019-08-21 17:37:41 +0530

    lab latest

--- AL/week4/q3.c
+++ AL/week4/q3.c
@@ -16,20 +16,32 @@ int V[50];
 
 void bfs(int start) {
 
-    printf("%c ", start + 65);
     V[start] = 1;
+
+    printf("%c ", start + 97);
 
     for(int i = start; i < n; i++) {
 
-        for(int j = 0; j < n; j++) {
+        int set_start = 1;
+
+        for(int j =0; j < n; j++) {
 
             if(!V[j] && mat[i][j] == 1) {
-                printf("%c ", j + 65);
-                V[j] = 1;
-            }
 
-            
+                V[j] = 1;
+                printf("%c ", j + 97);
+
+                if(set_start == 1) {
+                    start = j;
+                    set_start = 0;
+                }
+                
+
+             }
+
         }
+
+        
     }
 
     printf("\n");
