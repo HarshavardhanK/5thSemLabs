711cf052e133f3937eb08cfc741ed16d1e742725
tsarshah <harshavardhan816@gmail.com>
2019-08-21 17:17:32 +0530

    lab latest commit

---
+++ AL/week4/graph.c
@@ -0,0 +1,6 @@
+#include "graph.h"
+
+int main() {
+
+    printf("%ld\n", sizeof(Node*));
+}
--- AL/week4/q3.c
+++ AL/week4/q3.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 int mat[7][7] = {
                     { 0, 1, 1, 1 ,1,0,0}, 
-                    { 1, 0, 0, 1 ,0,0,0}, 
-                    { 1, 0, 0, 0 ,0,0,1}, 
-                    { 1, 0, 0, 0 ,0,1,0},
-                    { 1, 0, 0, 0, 0,0,1},
-                    { 0, 0, 0, 1, 0,0,0},
+                    { 1, 0, 0, 1 ,0,1,0}, 
+                    { 1, 0, 0, 0 ,1,0,1}, 
+                    { 1, 1, 0, 0 ,0,1,0},
+                    { 1, 0, 1, 0, 0,0,1},
+                    { 0, 1, 0, 1, 0,0,0},
                     { 0, 0, 1, 0, 1,0,0},
                    }; 
 
@@ -14,29 +14,26 @@ int n=7;
 int V[50];
 
 
-void dfs(int i)
-{
-    printf("%c\n",(i+65));
-    
-    for(int i=0;i<n;i++)
-    {
-        V[i]=1;
-        for(int j=0;j<n;j++)
-        {
+void bfs(int start) {
 
-            if(!V[j] && mat[i][j]==1)
-            {
-            
-                V[j]=1;
-                printf("%c\n",(j+65));
-           
+    printf("%c ", start + 65);
+    V[start] = 1;
+
+    for(int i = start; i < n; i++) {
+
+        for(int j = 0; j < n; j++) {
+
+            if(!V[j] && mat[i][j] == 1) {
+                printf("%c ", j + 65);
+                V[j] = 1;
             }
+
+            
         }
-
     }
-   
-}
 
+    printf("\n");
+}
 
 
 
@@ -46,11 +43,11 @@ int main()
 {
 
 
-    for(int k=0;k<n;k++)
+     for(int k=0;k<n;k++)
     {
         V[k]=0;
     }
 
-    dfs(0);
+    bfs(2);
     return 0;
 }
